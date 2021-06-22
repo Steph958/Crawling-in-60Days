89610106e40c8c895b43a99263ac04db5641ec2d
Steph958 <slony958@gmail.com>
2021-06-22 13:34:08 +0800

    2021.06.22

--- Homework/Day_28/Day028_Scrapy_API/myproject/myproject/spiders/PTTCrawler.py
+++ Homework/Day_28/Day028_Scrapy_API/myproject/myproject/spiders/PTTCrawler.py
@@ -3,16 +3,17 @@ import scrapy
 import re
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin, urlparse
-from pathlib import Path
 from pprint import pprint
+from pathlib import Path
 from ..items import PTTArticleItem
 
-# 範例目標網址: https://www.ptt.cc/bbs/NBA_Film/M.1624076374.A.116.html
 class PttcrawlerSpider(scrapy.Spider):
     name = 'PTTCrawler'
-    allowed_domains = ['www.ptt.cc']
-    start_urls = ['https://www.ptt.cc/bbs/NBA_Film/M.1624076374.A.116.html']
-    cookies = {'over18': '1'}
+    def __init__(self, start_urls, filename=None):
+        self.cookies = {'over18': '1'}
+        self.start_urls = start_urls
+        self.filename = filename
+        super().__init__()
 
     def start_requests(self):
         for url in self.start_urls:
@@ -31,7 +32,7 @@ class PttcrawlerSpider(scrapy.Spider):
         # 取得文章內容主體
         main_content = soup.find(id='main-content')
         
-        # 假如文章有屬性資料 (meta), 我們再從屬性的區塊中爬出作者 (author), 文章標題 (title), 發文日期 (date)
+        # 假如文章有屬性資料 (meta), 我們在從屬性的區塊中爬出作者 (author), 文章標題 (title), 發文日期 (date)
         metas = main_content.select('div.article-metaline')
         author = ''
         title = ''
@@ -45,6 +46,7 @@ class PttcrawlerSpider(scrapy.Spider):
                 date = metas[2].select('span.article-meta-value')[0].string
 
             # 從 main_content 中移除 meta 資訊（author, title, date 與其他看板資訊）
+            #
             # .extract() 方法可以參考官方文件
             #  - https://www.crummy.com/software/BeautifulSoup/bs4/doc/#extract
             for m in metas:
--- Homework/Day_28/Day029_Scrapy_PTT/myproject/myproject/spiders/PTTCrawler.py
+++ Homework/Day_28/Day029_Scrapy_PTT/myproject/myproject/spiders/PTTCrawler.py
@@ -7,18 +7,47 @@ from pathlib import Path
 from pprint import pprint
 from ..items import PTTArticleItem
 
-# 範例目標網址: https://www.ptt.cc/bbs/NBA_Film/M.1624076374.A.116.html
 class PttcrawlerSpider(scrapy.Spider):
     name = 'PTTCrawler'
-    allowed_domains = ['www.ptt.cc']
-    start_urls = ['https://www.ptt.cc/bbs/NBA_Film/M.1624076374.A.116.html']
-    cookies = {'over18': '1'}
+    def __init__(self, board='Gossiping'):
+        self.cookies = {'over18': '1'}
+        self.host = 'https://www.ptt.cc'
+        self.board = board
+        self.start_urls = 'https://www.ptt.cc/bbs/{}/index.html'.format(board)
+        super().__init__()
 
     def start_requests(self):
-        for url in self.start_urls:
-            yield scrapy.Request(url=url, callback=self.parse, cookies=self.cookies)
+        yield scrapy.Request(url=self.start_urls, callback=self.parse, cookies=self.cookies)
 
     def parse(self, response):
+        # 取得列表中的清單主體
+        soup = BeautifulSoup(response.text)
+        main_list = soup.find('div', class_='bbs-screen')
+
+        # 依序檢查文章列表中的 tag, 遇到分隔線就結束，忽略這之後的文章
+        for div in main_list.findChildren('div', recursive=False):
+            class_name = div.attrs['class']
+
+            # 遇到分隔線要處理的情況
+            if class_name and 'r-list-sep' in class_name:
+                self.log('Reach the last article')
+                break
+
+            # 遇到目標文章
+            if class_name and 'r-ent' in class_name:
+                div_title = div.find('div', class_='title')
+                a_title = div_title.find('a', href=True)
+                # 如果文章已經被刪除則跳過
+                if not a_title or not a_title.has_attr('href'):
+                    continue
+                article_URL = urljoin(self.host, a_title['href'])
+                article_title = a_title.text
+                self.log('Parse article {}'.format(article_title))
+                yield scrapy.Request(url=article_URL,
+                                     callback=self.parse_article,
+                                     cookies=self.cookies)
+
+    def parse_article(self, response):
         # 假設網頁回應不是 200 OK 的話, 我們視為傳送請求失敗
         if response.status != 200:
             print('Error - {} is not available to access'.format(response.url))
@@ -31,7 +60,7 @@ class PttcrawlerSpider(scrapy.Spider):
         # 取得文章內容主體
         main_content = soup.find(id='main-content')
         
-        # 假如文章有屬性資料 (meta), 我們再從屬性的區塊中爬出作者 (author), 文章標題 (title), 發文日期 (date)
+        # 假如文章有屬性資料 (meta), 我們在從屬性的區塊中爬出作者 (author), 文章標題 (title), 發文日期 (date)
         metas = main_content.select('div.article-metaline')
         author = ''
         title = ''
@@ -45,6 +74,7 @@ class PttcrawlerSpider(scrapy.Spider):
                 date = metas[2].select('span.article-meta-value')[0].string
 
             # 從 main_content 中移除 meta 資訊（author, title, date 與其他看板資訊）
+            #
             # .extract() 方法可以參考官方文件
             #  - https://www.crummy.com/software/BeautifulSoup/bs4/doc/#extract
             for m in metas:
